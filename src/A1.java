53ffd08be3c23c38fe31724ec0fc4b4dcf592bd7
LakkamTejasri <lakkamtejasri@gmail.com>
2018-09-12 19:28:37 +0530

    save to github

---
+++ src/A1.java
@@ -0,0 +1,40 @@
+class Student {
+	Student(int id,String name)
+	{
+		int id1=id;
+		String name1=name;
+		//System.out.println("2-par");
+	}
+	void m2(int a)
+	{
+		System.out.println("1-pare");
+	}
+	
+}
+class engStudent extends Student{
+	engStudent(int id,String name,int salary)
+	{
+		super(id,name);
+		int id1=id;
+		String name1=name;
+		int salary1=salary;
+		//System.out.println("child");
+		
+		
+	}
+	void m1()
+	{
+		System.out.println("0-child");
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		engStudent s=new engStudent(1,"aa",50);
+		Student s1=new Student(2,"bb");
+		s1.m2(10);
+		s.m1();
+
+	}
+
+}
